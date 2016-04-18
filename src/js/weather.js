b5552d7c174dc251c1a8760f21d9bbbd4bb617c1
Kevin Tang <ktang012@ucr.edu>
2016-04-18 15:08:56 -0700

    upated

---
+++ src/js/weather.js
@@ -0,0 +1,36 @@
+if ("geolocation" in navigator) {
+    $('#weather').show();
+}
+else {
+    $('#weather').hide();
+}
+
+$(document).ready(function() {
+    getWeather();
+    var weatherInterval = setInterval(getWeather, 600000);
+});
+
+function getWeather() {
+    navigator.geolocation.getCurrentPosition(function(position) {
+         loadWeather(position.coords.latitude+','+position.coords.longitude);
+    });
+}
+
+function loadWeather(location, woeid) {
+  $.simpleWeather({
+    location: location,
+    woeid: woeid,
+    unit: 'f',
+    success: function(weather) {
+      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+      html += '<li class="currently">'+weather.currently+'</li>';
+      html += '<li>'+weather.alt.temp+'&deg;C</li></ul>';  
+      
+      $("#weather").html(html);
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  });
+}
