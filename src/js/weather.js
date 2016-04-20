c95cfa3b575884aef0ac7eb7b7cb62f4d939d50d
Kevin Tang <ktang012@ucr.edu>
2016-04-20 02:51:22 -0700

    metric switch functionality

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,38 +1,91 @@
-if ("geolocation" in navigator) {
-    $('#weather').show();
-}
-else {
-    $('#weather').hide();
-}
-
-$(document).ready(function() {
-    getWeather();
-    var weatherInterval = setInterval(getWeather, 600000);
-
+$(document).ready(function () {
+    verifyGeolocation();  
+    loadSettings();
+    
+    var weatherInterval = setInterval(setWeather, 600000);
+    $('#weather_onoff').change(function() {
+        setWeather();
+    });
 });
 
-function getWeather() {
-    navigator.geolocation.getCurrentPosition(function(position) {
-         loadWeather(position.coords.latitude+','+position.coords.longitude);
+function verifyGeolocation() {
+    if ("geolocation" in navigator) {
+        $('#weather').show();
+    }
+    else {
+        $('#weather').hide();
+    }
+}
+
+
+/* Call initial setWeather() here due to asynchronicity of storage functions */
+function loadSettings() {
+    chrome.storage.sync.get('metric', function(result) {
+        current_metric = result.metric;
+        if (current_metric === 'fahrenheit') {
+            $('#weather_onoff').prop('checked', true);
+            setWeather();
+        }
+        else if (current_metric === 'celsius') {
+            $('#weather_onoff').prop('checked', false);
+            setWeather();
+        }
+        else {
+            console.log("Error fetching weather metrics");     
+        }
     });
 }
 
-function loadWeather(location, woeid) {
+function setWeather() {
+    var data = { metric: ''};
+    if ($('#weather_onoff').prop('checked')) {
+        data.metric = 'fahrenheit';
+        chrome.storage.sync.set(data, function() {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                loadFahrenheit(position.coords.latitude+','+position.coords.longitude);
+            });
+        });
+    }
+    else if (!($('#weather_onoff').prop('checked'))) {
+        data.metric = 'celsius';
+        chrome.storage.sync.set(data, function() {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                loadCelsius(position.coords.latitude+','+position.coords.longitude);
+            });
+        });
+    }
+}
+
+function loadFahrenheit(location, woeid) {
   $.simpleWeather({
     location: location,
     woeid: woeid,
     unit: 'f',
     success: function(weather) {
-      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
-      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
-      html += '<li class="currently">'+weather.currently+'</li>';
-      html += '<li>'+weather.alt.temp+'&deg;C</li></ul>';  
-
-      $("#weather").html(html);
+        var html = '<h2>' + weather.temp + '&deg;' + weather.units.temp;
+        html += '<img src="' + weather.thumbnail + '"></img></h2>';
+        $('#weather_display').html(html);
     },
     error: function(error) {
-      console.log("fail");
-      $("#weather").html('<p>'+error+'</p>');
+      console.log("Failed to load weather");
+      $('#weather').html('<p>'+error+'</p>');
+    }
+  });
+}
+
+function loadCelsius(location, woeid) {
+  $.simpleWeather({
+    location: location,
+    woeid: woeid,
+    unit: 'c',
+    success: function(weather) {
+        var html = '<h2>' + weather.temp + '&deg;' + weather.units.temp;
+        html += '<img src="' + weather.thumbnail + '"></img></h2>';
+        $('#weather_display').html(html);
+    },
+    error: function(error) {
+      console.log("Failed to load weather");
+      $('#weather').html('<p>'+error+'</p>');
     }
   });
 }
