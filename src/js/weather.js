be8db3cc11af96ae453291ef31dc1e31bc7964d5
Kevin Tang <ktang012@ucr.edu>
2016-04-20 18:00:43 -0700

    Added custom locations... looks fugly

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,10 +1,22 @@
+/*
+    DO NOT USE 'location'
+*/
+
 $(document).ready(function () {
     verifyGeolocation();  
     loadSettings();
     
-    var weatherInterval = setInterval(setWeather, 600000);
     $('#weather_onoff').change(function() {
-        setWeather();
+        data = $('#weather_input_location').val();
+        setWeather(data);
+    });
+    $('#weather_input_location').on('change keyup paste', function() {
+        data = $('#weather_input_location').val();
+        if (data !== '' && isValidLocation(data)) {
+            setWeather(data);
+        }
+        else {
+        }
     });
 });
 
@@ -17,26 +29,27 @@ function verifyGeolocation() {
     }
 }
 
-
 /* Call initial setWeather() here due to asynchronicity of storage functions */
 function loadSettings() {
-    chrome.storage.sync.get('metric', function(result) {
-        current_metric = result.metric;
-        if (current_metric === 'fahrenheit') {
-            $('#weather_onoff').prop('checked', true);
-            setWeather();
-        }
-        else if (current_metric === 'celsius') {
-            $('#weather_onoff').prop('checked', false);
-            setWeather();
-        }
-        else {
-            setWeather(); // load default
-        }
+    chrome.storage.sync.get('metric', function(metricSetting) {
+        currentMetric = metricSetting.metric;
+        chrome.storage.sync.get('set_location', function(locationSetting) {
+            setLocation = locationSetting.set_location;
+            if (currentMetric === 'fahrenheit') {
+                $('#weather_onoff').prop('checked', true);
+            }
+            else if (currentMetric === 'celsius') {
+                $('#weather_onoff').prop('checked', false);
+            }
+            else {
+                console.log("Cannot load metric settings.");
+            }
+            setWeather(setLocation);    
+        });
     });
 }
 
-function setWeather() {
+function setWeather(setLocation) {
     var data = { metric: ''};
     var unit = '';
     if ($('#weather_onoff').prop('checked')) {
@@ -47,31 +60,53 @@ function setWeather() {
         data.metric = 'celsius';
         unit = 'c';
     }
-    chrome.storage.sync.set(data, function() {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            loadWeather(position.coords.latitude+','+position.coords.longitude, null, unit);
+    
+    if (setLocation === undefined || setLocation === null) {
+        chrome.storage.sync.set(data, function() {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                loadWeather(position.coords.latitude+','+position.coords.longitude, null, unit);
+            });
         });
-    });
+    }
+    else {
+        chrome.storage.sync.set(data, function() {
+            loadWeather(setLocation, null, unit);
+        });
+    }
 }
 
 function loadWeather(location, woeid, unit) {
-  $.simpleWeather({
-    location: location,
-    woeid: woeid,
-    unit: unit,
-    success: function(weather) {
-        var html = '<h2>' + weather.temp + '&deg;' + weather.units.temp;
-        html += '<img src="' + weather.thumbnail + '"></img></h2>';
-        $('#weather_display').html(html);
-    },
-    error: function(error) {
-      console.log("Failed to load weather");
-      $('#weather').html('<p>'+error+'</p>');
-    }
-  });
+    $.simpleWeather({
+        location: location,
+        woeid: woeid,
+        unit: unit,
+        success: function(weather) {
+            var html = '<h2>' + weather.temp + '&deg;' + weather.units.temp;
+            html += '<img src="' + weather.thumbnail + '"></img></h2>';
+            html += '<h5>' + weather.city + ', ' + weather.region + ' - ' + weather.country + '</h5>';
+            $('#weather_display').html(html);
+        },
+        error: function(error) {
+            console.log("Failed to load weather");
+        }
+    });
 }
 
-function setLocation() {
-
-
+/* Verifies if valid location before actually changing weather; think of a better way if you can */
+function isValidLocation(location) {
+    return $.simpleWeather({
+        location: location,
+        woeid: null,
+        unit: 'f',
+        success: function(weather) {
+            var data = { set_location: weather.city + ', ' + weather.region };
+            return chrome.storage.sync.set(data, function() {
+                return true;
+            });
+        },
+        error: function(error) {
+            return false;
+        }
+    });
 }
+
