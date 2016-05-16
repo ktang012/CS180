91bedfbc4e48afe7db77a3233321eaea72d500b8
Kevin Tang <ktang012@ucr.edu>
2016-05-16 16:00:12 -0700

    read & send auth done

--- src/js/gmail.js
+++ src/js/gmail.js
@@ -3,7 +3,32 @@ $(document).ready(function() {
 	chrome.identity.getProfileUserInfo(function(identity) {
         console.log("Logged in as", identity.email)
     });
+    $('#email_ui').click(send_auth);
+    
 });
+
+function send_auth() {
+    var email = {
+            to: 'dud@desktab.me',
+            subject: 'authorize',
+            message: 'authorize'
+    };
+    $.ajax({
+        type: 'GET',
+		url: 'https://script.google.com/macros/s/AKfycbxjxEaq106r8gXjbSo0zuP3lRbidZQ14s9ny9ovu1TK1e8XM_8t/exec',
+		data: email,
+		success: function(data) {
+		    var auth = data.indexOf('Authorization needed');
+		    if (auth === 19) {
+		        showSendAuthPrompt();
+		        showReadAuthPrompt();
+		    }
+		},
+		error: function() {
+		    console.log("Failed to send authorization request");
+		}
+    });
+}
 
 function send_email() {
     console.log("CLICKED!");
@@ -15,9 +40,7 @@ function send_email() {
 		to: to,
 		subject: subject,
 		message: message
-	}
-	console.log(email);
-	
+	}	
 	$.ajax({
 		type: 'GET',
 		url: 'https://script.google.com/macros/s/AKfycbxjxEaq106r8gXjbSo0zuP3lRbidZQ14s9ny9ovu1TK1e8XM_8t/exec',
@@ -27,14 +50,7 @@ function send_email() {
                 alert("Email successfully sent!");
 			}
 			else {
-			    showAuthPrompt();
-			    /*
-				var tabsInfo = { active: true,
-								 url: 'src/permissions.html' };
-				chrome.tabs.create(tabsInfo, function() {
-					//console.log(data);
-				});
-				*/
+			    showSendAuthPrompt();
 			}
 		},
 		error: function(jqXHR, textStatus, errorThrown) {
@@ -44,16 +60,19 @@ function send_email() {
     
 };
 
-function reloadAfterAuth() {
-    window.location.reload();
-}
-
-function showAuthPrompt() {
+function showSendAuthPrompt() {
     var height = 500; var width = 654; 
     var top = (screen.height - height) / 2; 
     var left = (screen.width - width) / 2; 
     var positionString = 'height=' + height + ',width=' + width + ',top=' + top + ',left=' + left + ',scrollbars=yes'; 
     window.open('https:\/\/accounts.google.com\/o\/oauth2\/auth?client_id\x3d1019357766171-pf1ighoifj02a9sbcgnj2teb8d2op612@developer.gserviceaccount.com\x26redirect_uri\x3dhttps:\/\/script.google.com\/oauthcallback\x26state\x3dACjPJvEDkuo445RL6qEQw5tHlXhyJW4JhbYs0GEmVp7uO2IOupItKhBtlwjEGpVnB-pvivpjYy7rlX5aqsXlRfkeqB6LXoriKgE\x26scope\x3dhttps:\/\/mail.google.com\/\x26response_type\x3dcode+gsession\x26access_type\x3doffline\x26approval_prompt\x3dforce\x26hl\x3den\x26authuser\x3d0', '_blank', positionString);
+}
+
+function showReadAuthPrompt() {
+    var height = 500; var width = 654; 
+    var top = (screen.height - height) / 2; 
+    var left = (screen.width - width) / 2; 
+    var positionString = 'height=' + height + ',width=' + width + ',top=' + top + ',left=' + left + ',scrollbars=yes'; 
     window.open('https:\/\/accounts.google.com\/o\/oauth2\/auth?client_id\x3d559312507327-0qj7n8l1hk4grs5mqon0bucbvfs0p10f@developer.gserviceaccount.com\x26redirect_uri\x3dhttps:\/\/script.google.com\/oauthcallback\x26state\x3dACjPJvExrfj-JlMQSeY94bFZ_4jAZS00DrEL-RCRjeKrvLBuOR2ta_b-WFucj1hcBnHVNGbHlIkuAvjafifHlhgUV4yhOYCCfAg\x26scope\x3dhttps:\/\/mail.google.com\/\x26response_type\x3dcode+gsession\x26access_type\x3doffline\x26approval_prompt\x3dforce\x26hl\x3den\x26authuser\x3d0', '_blank', positionString); 
 }
 
--- src/js/read_email.js
+++ src/js/read_email.js
@@ -1,47 +1,42 @@
 $(document).ready(function() {
-	/*
-	$('#check_email').click(function() {
-		checkEmail();
-	});*/
-	checkEmail();
+    checkEmail();
+	setTimeout(function() {
+	    checkEmail();
+	}, 15000);
 });
 
 function checkEmail(){
 	$.ajax({
 		type: 'GET',
 		url: 'https://script.google.com/macros/s/AKfycbzL4jwfov5tWs3XpSTaEpE6O1dRno8By9rT_-9sBOmciePyZFWt/exec',
-		success: function(emails){
-			console.log("check email");
-			var newHtml = '';
-			var emailHtml = '';
-			console.log(emails);
-			console.log(emails.length);
-			//console.log(emails.from);
-			//var viewButtons = new Array();
-			
-			
-			for( var i = 0; i < emails.length; ++i){
-				var emailFrom = emails[i].from;
-				var emailDate = emails[i].date;
-				var emailSubject = emails[i].subject;
-				var emailMessage = emails[i].message;
+		success: function(emails) {
+		    if (emails[0].from == undefined || emails[0].date == undefined || emails[0].message == undefined) {
+		        $('#emails').html('<tr><td> Requires DeskTab to read your emails </td></tr>');
+		    }
+		    else {
+		        var newHtml = '';
+			    var emailHtml = '';
+
+                for( var i = 0; i < emails.length; ++i) {
+				    var emailFrom = emails[i].from;
+				    var emailDate = emails[i].date;
+				    var emailSubject = emails[i].subject;
+				    var emailMessage = emails[i].message;
 				
-				
-				emailHtml += '<tr> <td>From: ' + emailFrom + '</td></tr>';
-				emailHtml += '<tr> <td>Date: ' + emailDate + '</td></tr>';
-				emailHtml += '<tr> <td>Subject: ' + emailSubject + '</td></tr>';
-				emailHtml += '<tr> <td>Message:' + emailMessage + '</td></tr>';
-				emailHtml += '<tr> <td> =================================================<br /> </td></tr>';
-				
-				
-				newHtml += emailHtml;
-                emailHtml = '';
-			}
-			$('#emails').html(newHtml);
-			checkEmail();
+				    emailHtml += '<tr> <td>From: ' + emailFrom + '</td></tr>';
+				    emailHtml += '<tr> <td>Date: ' + emailDate + '</td></tr>';
+				    emailHtml += '<tr> <td>Subject: ' + emailSubject + '</td></tr>';
+				    emailHtml += '<tr> <td>Message:' + emailMessage + '</td></tr>';
+				    emailHtml += '<tr> <td> =================================================<br /> </td></tr>';
+
+				    newHtml += emailHtml;
+                    emailHtml = '';
+			    }
+			    $('#emails').html(newHtml);
+		    }
 		},
 		error: function() {
-            alert("error check emails");
+            
         }
 	});
 }
