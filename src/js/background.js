6645ac07b677731c193bd1d2634e776faaf59a67
Kevin Tang <ktang012@ucr.edu>
2016-05-21 00:16:40 -0700

    fixed background bug

--- src/js/background.js
+++ src/js/background.js
@@ -1,36 +1,22 @@
 $(document).ready(function() {
+    var header = $('body');
+    var backgrounds = new Array(
+        'url(img/img1.jpg)',
+        'url(img/img2.jpg)',
+        'url(img/img3.jpg)',
+        'url(img/img4.jpg)',
+        'url(img/img5.jpg)',
+        'url(img/img6.jpg)',
+        'url(img/img7.jpg)'
+    );
 
-
-var header = $('body');
-
-var backgrounds = new Array(
-    'url(img/img1.jpg)'
-  , 'url(img/img2.jpg)'
-  , 'url(img/img3.jpg)'
-  , 'url(img/img4.jpg)'
-  , 'url(img/img5.jpg)'
-  , 'url(img/img6.jpg)'
-  , 'url(img/img7.jpg)'
-);
-
-var current = 0;
-
-function nextBackground() {
-    current++;
-    current = current % backgrounds.length;
-    header.css('background-image', backgrounds[current]);
-	header.css('background-size', '100% 1590%');
-	header.css('background-repeat', 'no-repeat');
-	
-	
-	
-}
-
-
-setInterval(nextBackground, 180000);
-
-header.css('background-image', backgrounds[current]);	
-header.css('background-size', '100% 1590%');
-header.css('background-repeat', 'no-repeat');
-
+    var current = Math.floor(Math.random() * 100);
+    function nextBackground() {
+        current = Math.floor(Math.random() * 100);
+        header.css('background-image', backgrounds[current % backgrounds.length]);
+    }
+    setInterval(nextBackground, 180000);
+    
+    
+    header.css('background-image', backgrounds[current % backgrounds.length]);
 });
